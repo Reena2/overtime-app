05a807dbc269d4e56542d951a82a7fc6c0e10fd4
isaichenko <isaichenko.e@gmail.com>
2018-03-07 16:34:50 +0200

    Table

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,16 +1,16 @@
-User.create!(email: "test3@test.com",
+@user = User.create!(email: "test3@test.com",
              password: "123123",
              password_confirmation: "123123",
              first_name: "Jon",
              last_name: "Snow"
              )
 
-
+puts "User have been created"
 
 100.times do |post|
   Post.create!(date: Date.today,
     rationale: "#{post} rationale content",
-    user_id: User.last.id)
+    user_id: @user.id)
 end
 
 puts "100 Posts have been created"
