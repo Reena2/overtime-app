dae4999122af0d0f5ff380234b3a36246d18c1c1
isaichenko <isaichenko.e@gmail.com>
2018-03-12 23:33:00 +0200

    Implemented edit functionality for status

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,6 +7,15 @@
 
 puts "User have been created"
 
+AdminUser.create!(email: "test@test.com",
+             password: "123123",
+             password_confirmation: "123123",
+             first_name: "Admin",
+             last_name: "User"
+             )
+
+puts "Admin User have been created"
+
 100.times do |post|
   Post.create!(date: Date.today,
     rationale: "#{post} rationale content",
