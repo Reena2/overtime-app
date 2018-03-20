91b994efd071ebc74f898f399c24173d8dcb2852
isaichenko <isaichenko.e@gmail.com>
2018-03-20 17:48:35 +0200

    Sms functional

--- lib/sms_tool.rb
+++ lib/sms_tool.rb
@@ -1,5 +1,16 @@
 module SmsTool
+  account_sid = ENV['TWILIO_ACCOUNT_SID']
+  auth_token = ENV['TWILIO_AUTH_TOKEN']
+
+  @client = Twilio::REST::Client.new account_sid, auth_token
+
   def self.send_sms(number:, message:)
+    @client.api.account.messages.create(
+      from: '+14159341234',
+      to: '+16105557069',
+      body: 'Hey there!'
+    )
+
     puts "Sending SMS..."
     puts "#{message} to #{number}"
   end
